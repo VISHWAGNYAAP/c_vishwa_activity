fbe0463e72a3d48b4f5cecd827396c5dd7c6b90a
VISHWAGNYAAP <vishwagnyaap.bsc23@rvu.edu.in>
2024-01-24 21:00:48 +0530

    test

--- set01/problem08.c
+++ set01/problem08.c
@@ -8,33 +8,33 @@ int input_array_size()
 }
 void input_array(int n,int a[n])
 {
-    for(int i=0;i<=n;i++)
+    printf("ENTER %d ELEMENTS FOR THE ARRAY: ", n);
+    for(int i=0;i<n;i++)
     {
-        scanf("%d",&a[n]);
+        scanf("%d",&a[i]);
     }
 }
-int sum_n_array(int n,int a[n],int sum)
+int sum_n_array(int n,int a[n])
 {
     
     int sum=0;
-    for(int i=1;i<=n;i++)
+    for(int i=0;i<n;i++)
     {
-        sum +=i;
+        sum +=a[i];
     }
     return sum;
 }
 void output(int n,int a[n],int sum)
 {
-    printf("THE SUM IS %d",sum);
+    printf("THE SUM IS %d\n",sum);
 }
 int main()
 {
-    int n,result;
+    int n,sum;
     n=input_array_size();
-    int a[n]=input_array();
-    sum_n_array(n,a[n],sum);
-    int sum;
-    sum=sum_n_array(n,a[n]);
-    output(n,a[n],sum);
+    int a[n];
+    input_array(n,a);
+    sum=sum_n_array(n,a);
+    output(n,a,sum);
     return 0;
 }
