56d9747111fd9cb8297ec7aa48aea45a56905692
VISHWAGNYAAP <vishwagnyaap.bsc23@rvu.edu.in>
2024-01-24 18:52:24 +0530

    test

---
+++ set01/problem07.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int input_n()
+{
+    int n;
+    printf("ENTER THE VALUE OF N");
+    scanf("%d",&n);
+    return n;
+}
+int sum_n_nos(int n)
+{
+    int result=0;
+    for(int i=1;i<=n;i++)
+    {
+        result +=i;
+    }
+    return result;
+}
+void output(int n,int sum)
+{
+    printf("THE SUM FROM 1 TO %d IS %d",n,sum);
+}
+int main()
+{
+    int n=input_n();
+    int total=sum_n_nos(n);
+    output(n,total);
+    return 0;
+}
