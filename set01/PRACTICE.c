229f7f54344041eba84cba69901ea5cd5301fb06
VISHWAGNYAAP <vishwagnyaap.bsc23@rvu.edu.in>
2024-02-27 14:44:59 +0530

    TEST

--- set01/PRACTICE.c
+++ set01/PRACTICE.c
@@ -6,8 +6,19 @@ int input(int *a,int *b)
     printf("ENTER THE NUMBERS TO BE ADDED:");
     scanf("%d",& *b);
 }
-void add(int a,int b,int sum)
+void add(int a,int b,int *sum)
 {
     *sum = a+b;
 }
-void
+void output(int a,int b, int sum)
+{
+    printf("THE SUM OF THE NUMBERS %d AND %d IS %d",a,b,sum);
+}
+int main()
+{
+    int a,b,sum;
+    input(&a,&b);
+    add(a,b,&sum);
+    output(a,b,sum);
+    return 0;
+}
