084d86a9d3a7a8177a62c18c613cdb0063b097ad
VISHWAGNYAAP <vishwagnyaap.bsc23@rvu.edu.in>
2024-02-29 21:51:11 +0530

    TEST

--- set01/PRACTICE.c
+++ set01/PRACTICE.c
@@ -1,39 +1,6 @@
 #include <stdio.h>
-int input()
+int input(int *a,int *b ,int *c)
 {
-    int n;
-    printf("ENTER THE NUMBERS TO BE COMAPRED:");
-    scanf("%d%d%d",&n);
-    return n;
-}
-int compare(int a,int b,int c)
-{
-    int largest;
-    if((a>=b)&&(a>=c))
-    {
-        largest == a;
-    }
-    if(b>=c)
-    {
-       largest == b;
-    }
-    else
-    {
-        largest == c;
-    }
-    return largest;
-}
-void output(int a,int b,int c,int largest)
-{
-    printf("THE LARGEST AMONG %d,%d AND %d IS %d",a,b,c,largest);
-}
-int main()
-{
-    int num1,num2,num3,largest;
-    num1=input();
-    num2=input();
-    num3=input();
-    largest=compare(num1,num2,num3);
-    output(num1,num2,num3,largest);
-    return 0;
+    printf("ENTER THE FIRST NUMBER TO BE COMPARED:");
+    scanf("%d",&)
 }
