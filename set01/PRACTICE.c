951255567e60777798ab2c7b523b78cea50d70b3
VISHWAGNYAAP <vishwagnyaap.bsc23@rvu.edu.in>
2024-03-01 09:45:07 +0530

    TEST

--- set01/PRACTICE.c
+++ set01/PRACTICE.c
@@ -2,5 +2,38 @@
 int input(int *a,int *b ,int *c)
 {
     printf("ENTER THE FIRST NUMBER TO BE COMPARED:");
-    scanf("%d",&)
+    scanf("%d",&a,&b,&c);
+    return a;
+
+}
+int compare(int a,int b,int c)
+{
+int largest;
+if((a>=b)&&(a>=c))
+{
+    largest == a;
+}
+if(b>=c)
+{
+    largest == b;
+}
+else 
+{
+    largest == c;
+}
+return largest;
+}
+void output(int a,int b,int c,int largest)
+{
+    printf("THE LARGEST AMONG THE NUMBERS %d,%d,%d IS %d",a,b,c,largest);
+}
+int main()
+{
+    int num1,num2,num3,largest;
+    num1 = input();
+    num2 = input();
+    num3 = input();
+    largest=compare(num1,num2,num3);
+    output(num1,num2,num3,largest);
+    return 0; 
 }
